0ab8a055d67b7dce40353ff19924db6b850bb045
afshinhatampour <afshin89hatampour@gmail.com>
2019-07-05 21:43:47 +0430

    relationship between products and reviews

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -3,8 +3,14 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Model\Review;
 
 class Product extends Model
 {
     protected  $fillable = ['name', 'detail', 'price', 'stock', 'discount'];
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -3,8 +3,14 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Model\Product;
 
 class Review extends Model
 {
     protected  $fillable = ['product_id', 'customer', 'review', 'star'];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
