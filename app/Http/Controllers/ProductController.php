1c86039e885ba382551a85dd4c4c25cf893743b5
afshinhatampour <afshin89hatampour@gmail.com>
2019-07-06 22:42:01 +0430

    Product Resource created

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\Product\ProductResource;
 use App\Model\Product;
 use Illuminate\Http\Request;
 
@@ -14,7 +15,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        return Product::all();
     }
 
     /**
@@ -30,7 +31,8 @@ class ProductController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
+     *
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -41,18 +43,20 @@ class ProductController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Model\Product  $product
+     * @param  \App\Model\Product $product
+     *
      * @return \Illuminate\Http\Response
      */
     public function show(Product $product)
     {
-        //
+        return new ProductResource($product);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Model\Product  $product
+     * @param  \App\Model\Product $product
+     *
      * @return \Illuminate\Http\Response
      */
     public function edit(Product $product)
@@ -63,8 +67,9 @@ class ProductController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Model\Product  $product
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Model\Product       $product
+     *
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Product $product)
@@ -75,7 +80,8 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Model\Product  $product
+     * @param  \App\Model\Product $product
+     *
      * @return \Illuminate\Http\Response
      */
     public function destroy(Product $product)
