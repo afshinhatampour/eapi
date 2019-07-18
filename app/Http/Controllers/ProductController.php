8d3c00fd527ff2e8ea57ebc927a4ef6bdb9f8221
afshinhatampour <afshin89hatampour@gmail.com>
2019-07-18 21:00:14 +0430

    product destroy

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -109,6 +109,9 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        return response([
+            "success" => "product delted successfully"
+        ]);
     }
 }
